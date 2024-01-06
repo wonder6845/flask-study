30b42f1e4d4ded863ba2d66f2aa69a57057739b9
root <root@DESKTOP-5ED72JU>
2024-01-06 14:20:14 +0900

    step1: minitwit-add user-info

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+app.users = {}
+app.id_count = 1
+
+@app.route("/ping", methods=['GET'])
+def ping():
+    return "pong"
+
+@app.route("/sign-up", methods=['POST'])
+def sign_up():
+    new_user = request.json
+    new_user = app.id_count
+    app.users[app.id_count] = new_user
+    app.id_count = app.id_count + 1
+    
+    return jsonify(new_user)
+
